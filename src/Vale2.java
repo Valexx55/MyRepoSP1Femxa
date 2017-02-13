911062f2a58aca290e7b59f7c0302470203d3e77
valexx55 <valexx55@gmail.com>
2017-02-13 18:01:37 +0100

    Método Saludo

--- src/Vale2.java
+++ src/Vale2.java
@@ -1,6 +1,17 @@
 
 public class Vale2 {
 	
+	public int saludo (String nombre)
+	{
+		int nletrassaludo = 0;
+		
+		System.out.println("Hola " + nombre);
+		
+		return nletrassaludo;
+	}
+	
+	
+	
 	public static void main(String[] args) {
 		
 		System.out.println("Buenos días");
