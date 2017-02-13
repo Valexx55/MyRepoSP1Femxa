2b12dab519a2b7027ab24ce01c3cacb3528b3133
alvgarfer <alvgarfer@gmail.com>
2017-02-13 18:38:49 +0100

    Update

--- src/Vale2.java
+++ src/Vale2.java
@@ -16,17 +16,27 @@ public class Vale2 {
 	
 	
 	
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		
 		System.out.println("Buenos d�as");
 
 	}
 	
-	public static void metodoDuque (){
-		
-		System.out.println("Buenas tardes Valeriano");
-		
-		
+	public static void metodoDuque ()
+	{
+		System.out.println("Buenas tardes Valeriano");		
 	}
 
+	public static String UnirCadenas (String cadena1, String cadena2)
+	{
+		String frase_junta = null;
+		
+		String juntarcadena1 = cadena1.replace (" ","");
+		String juntarcadena2 = cadena2.replace (" ","");
+		
+		frase_junta = juntarcadena1 + juntarcadena2;
+		
+		return frase_junta;
+	}
 }
