5840d0c4d1f5bb5e1677174cbcf922d133a3d463
zaineb88 <z.m.malak@hotmail.com>
2017-02-13 19:59:07 +0100

    por zaineb

--- src/Vale2.java
+++ src/Vale2.java
@@ -35,7 +35,7 @@ public class Vale2 {
 		frase_junta = cadena1 + cadena2;
 		cadena2.replace(" ","");
 		cadena1=cadena1.replace(" ", "");
-//cambios mezclados		
+//por zaineb 
 
 		return frase_junta;
 	}
